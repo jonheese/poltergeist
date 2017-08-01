ce0eeeaa9c9bb9a480d5706201fd25a67fd41597
Jon Heese <jheese@inetu.net>
2017-08-01 16:40:50 -0400

    Fix install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,21 @@
 
 # This will probably only work 100% properly on Ubuntu 14.04 (like what's installed on the VW-East RasPis)
 
+error=0
+
 cp apache-confs/* /etc/apache2/sites-available/
 cp -r webdirs/* /var/www/
 
-for site in $(ls apache-confs/*.conf | sed -i 's/\.conf//'); do
-	a2ensite $site
+cd apache-confs
+for site in $(ls *.conf | sed 's/\.conf//g'); do
+	a2ensite $site >/dev/null 2>&1
+	[ $? -ne 0 ] && echo "Error enabling $site site!" && error=1
 done
+cd ..
 
-service apache2 reload
+if [ $error -eq 0 ] ; then
+	service apache2 reload
+	echo "Sites successfully deployed!"
+else
+	echo "Sites not deployed!"
+fi
