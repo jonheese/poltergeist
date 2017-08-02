a0e01a6da286f52931d40bdb68f185faa262c6fc
Jon Heese <jheese@inetu.net>
2017-08-02 09:22:29 -0400

    Make deploy.sh work remotely, add documentation

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,24 @@
 
 error=0
 
-cp apache-confs/* /etc/apache2/sites-available/
-cp -r webdirs/* /var/www/
+if [ -n "$1" ] ; then
+	ssh="ssh $1"
+	dest="${1}:"
+fi
+
+rsync -av apache-confs/* ${dest}/etc/apache2/sites-available/
+rsync -av webdirs/* ${dest}/var/www/
 
 cd apache-confs
 for site in $(ls *.conf | sed 's/\.conf//g'); do
-	a2ensite $site >/dev/null 2>&1
+	$ssh a2ensite $site >/dev/null 2>&1
 	[ $? -ne 0 ] && echo "Error enabling $site site!" && error=1
 done
 cd ..
 
 if [ $error -eq 0 ] ; then
-	service apache2 reload
+	$ssh service apache2 reload
+	[ $? -ne 0 ] && echo "Error reloading apache2 service!" && exit 1
 	echo "Sites successfully deployed!"
 else
 	echo "Sites not deployed!"
--- readme.md
+++ readme.md
@@ -1,6 +1,17 @@
-To install:
+# Poltergeist
+Makes TVs talk to you
+
+## Installation
+Installing locally:
 ```
 git clone git@code.viawest.net:jon.heese/poltergeist.git
 cd poltergeist
-./install.sh
+./deploy.sh
+```
+
+Installing remotely:
+```
+git clone git@code.viawest.net:jon.heese/poltergeist.git
+cd poltergeist
+./deploy.sh <IP-or-HOSTNAME>
 ```
