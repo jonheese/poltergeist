77d258bd73a86e5d0b0f93ff757b960b3c3e1b07
Jon Heese <jheese@inetu.net>
2017-08-07 14:39:15 -0400

    Fix doitlive

--- webdirs/doitlive/index.php
+++ webdirs/doitlive/index.php
@@ -4,9 +4,9 @@
 		<title>We'll Do It Live!</title>
 	</head>
 	<body>
-		<p align="center"><img height='100%' src='doitlive.jpg' /></p>
+		<p align="center"><img height='300px' src='doitlive.jpg' /></p>
 <?php
-exec('/usr/bin/play /var/www/doitlive.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+exec('/usr/bin/play /var/www/doitlive/doitlive.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
 ?>
 	</body>
 </html>
