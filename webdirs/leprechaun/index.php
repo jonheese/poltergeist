05565860158e6b7e634f75d844cb92885a748330
Jon Heese <jheese@inetu.net>
2017-08-16 10:32:15 -0400

    Add leprechaun and iwantthegold sites

---
+++ webdirs/leprechaun/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="leprechaun-small.png" />
+		<title>Who All See The Leprechaun Say Yeah!</title>
+	</head>
+	<body>
+		<p align="center"><img src='leprechaun.png' /></p>
+<?php
+exec('/usr/bin/play /var/www/leprechaun/leprechaun.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
