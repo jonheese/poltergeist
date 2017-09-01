19415dc0c5139899cbc0226263c21d4b13e282f1
Jon Heese <jheese@inetu.net>
2017-09-01 09:19:10 -0400

    Add ow site

---
+++ webdirs/ow/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="ow.png" />
+		<title>Ooh!</title>
+	</head>
+	<body>
+		<p align="center"><img height='100%' src='ow.jpg' /></p>
+<?php
+exec('/usr/bin/play /var/www/ow/ow.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
