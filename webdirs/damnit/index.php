be6d39870eb0f6e61d7c8a46ae197b8f72910af6
Jon Heese <jheese@inetu.net>
2017-09-15 14:37:32 -0400

    Add damnit site

---
+++ webdirs/damnit/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<!--<link rel="shortcut icon" href="ow.png" />-->
+		<title>Damn it, Michael!</title>
+	</head>
+	<body>
+		<!--<p align="center"><img src='ow.jpg' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/damnit/damnit.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
