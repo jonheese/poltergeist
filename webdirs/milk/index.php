cbf5ee1039033e1287a412de586c61c850523803
Jon Heese <jheese@inetu.net>
2017-09-26 11:59:38 -0400

    Add milk site

---
+++ webdirs/milk/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="milk.jpg" />
+		<title>He need some milk!</title>
+	</head>
+	<body>
+		<p align="center"><img height='100%' src='milk.png' /></p>
+<?php
+exec('/usr/bin/play /var/www/milk/milk.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
