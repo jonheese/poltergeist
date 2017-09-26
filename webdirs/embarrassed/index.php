0208ad10b022039eed6788ebbe959a1665e30770
Jon Heese <jheese@inetu.net>
2017-09-26 13:47:52 -0400

    Fix typos

--- webdirs/embarrassed/index.php
+++ webdirs/embarrassed/index.php
@@ -1,10 +1,10 @@
 <html>
 	<head>
-		<link rel="shortcut icon" href="embarrased.jpg" />
+		<link rel="shortcut icon" href="embarrassed.jpg" />
 		<title>You must be so embarrassed!</title>
 	</head>
 	<body>
-		<p align="center"><img src='emabarrassed.gif' /></p>
+		<p align="center"><img src='embarrassed.gif' /></p>
 <?php
 exec('/usr/bin/play /var/www/embarrassed/embarrassed.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
 ?>
