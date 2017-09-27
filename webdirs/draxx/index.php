b902f454091f97cd49b643c29b6539571b3d9ee5
Jon Heese <jheese@inetu.net>
2017-09-27 14:43:00 -0400

    Add draxx site

---
+++ webdirs/draxx/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="draxx.jpg" />
+		<title>Draxx Them Sklounst</title>
+	</head>
+	<body>
+		<p align="center"><img src='draxx.webp' /></p>
+<?php
+exec('/usr/bin/play /var/www/draxx/draxx.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
