334cf375060a5ba4f6a9c66f9b509ce66c00c8b7
Jon Heese <jheese@inetu.net>
2017-09-29 09:21:16 -0400

    Add nerds site

---
+++ webdirs/nerds/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="nerds.png" />
+		<title>NERDS!</title>
+	</head>
+	<body>
+		<p align="center"><img src='nerds.jpg' /></p>
+<?php
+exec('/usr/bin/play /var/www/nerds/nerds.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
