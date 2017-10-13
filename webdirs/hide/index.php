3fd59017e8d6c6662dd6c574f0353a52d7d73709
Jon Heese <jheese@inetu.net>
2017-10-13 09:27:04 -0400

    Add new site hide

---
+++ webdirs/hide/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="hide.png" />
+		<title>Hide da money, y'all!</title>
+	</head>
+	<body>
+		<p align="center"><img src='hide.jpg' /></p>
+<?php
+exec('/usr/bin/play /var/www/hide/hide.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
