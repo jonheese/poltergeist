130c3ab2f48d6cfba2c9e6beaa368871e4d8e557
Jon Heese <jheese@inetu.net>
2017-10-17 15:45:39 -0400

    Fix typo in misc_setup.sh

--- misc_setup.sh
+++ misc_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chown www-data:www-data /var/www/* 2>/dev/null
 if [ ! -f /var/www/hostile/level.txt ] ; then
     touch /var/www/hostile/level.txt
-    echo "!" > /var/www/hostile/level.txt
+    echo "1" > /var/www/hostile/level.txt
 fi
+chown -R www-data:www-data /var/www/* 2>/dev/null
