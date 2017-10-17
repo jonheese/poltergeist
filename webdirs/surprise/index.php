7d85a8358477adebcad1553da68bb6edabfce02a
Jon Heese <jheese@inetu.net>
2017-10-17 14:58:32 -0400

    Add hostile and surprise sites

---
+++ webdirs/surprise/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Surprise Mother F**ker!</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/surprise/surprise.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
