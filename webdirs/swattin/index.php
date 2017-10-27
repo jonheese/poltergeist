474069df96a3300df13f7c3fe0b3fc5de5a95ae8
Jon Heese <jheese@inetu.net>
2017-10-27 16:10:29 -0400

    Add swattin site

---
+++ webdirs/swattin/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>What you swattin' at?</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/swattin/swattin.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
