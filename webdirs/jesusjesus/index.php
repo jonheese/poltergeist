224ea45b37f009bd868122cbf2359f5e8d01d218
Jon Heese <jheese@inetu.net>
2017-11-03 10:41:34 -0400

    Fix typos

--- webdirs/jesusjesus/index.php
+++ webdirs/jesusjesus/index.php
@@ -4,7 +4,7 @@
 	</head>
 	<body>
 <?php
-exec('/usr/bin/play /var/www/jesus/jesus.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+exec('/usr/bin/play /var/www/jesusjesus/jesusjesus.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
 ?>
 	</body>
 </html>
