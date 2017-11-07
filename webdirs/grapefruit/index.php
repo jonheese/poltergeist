cbe7f84995aa4b11f81efccaa3434bdb8db25016
Jon Heese <jheese@inetu.net>
2017-11-07 15:49:01 -0500

    Add grapefruit site

---
+++ webdirs/grapefruit/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<link rel="shortcut icon" href="grapefruit.png" />
+		<title>Grapefruit</title>
+	</head>
+	<body>
+		<p align="center"><img src='grapefruit.jpg' /></p>
+<?php
+exec('/usr/bin/play /var/www/grapefruit/grapefruit.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
