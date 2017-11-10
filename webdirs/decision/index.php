a3a3172ad321799fd61ccc07f2622dc0945e30d9
Jon Heese <jheese@inetu.net>
2017-11-10 11:34:35 -0500

    Add decision site

---
+++ webdirs/decision/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>Stupid Ass Decision</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/decision/decision.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
