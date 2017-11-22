5a6e1635bddd1ec21a4d00e1181102213212a2c6
Jon Heese <jheese@inetu.net>
2017-11-22 08:55:36 -0500

    Add coldblooded site

---
+++ webdirs/coldblooded/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Cold Blooded</title>
+	</head>
+	<body>
+       <p align="center"><img src='coldblooded.jpg' /></p>
+<?php
+exec('/usr/bin/play /var/www/coldblooded/coldblooded.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
