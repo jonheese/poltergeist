6b2111bb72b75da3ef8b7b4c33643d9aff0df867
Jon Heese <jheese@inetu.net>
2017-12-06 15:53:36 -0500

    Fix image sizes

--- webdirs/bobs/index.php
+++ webdirs/bobs/index.php
@@ -3,7 +3,7 @@
 		<title>The Bobs</title>
 	</head>
 	<body>
-		<p align="center"><img height='100%' src='bobs.jpg' /></p>
+		<p align="center"><img src='bobs.jpg' /></p>
 <?php
 $bob_number = rand(1,5);
 exec("/usr/bin/play /var/www/bobs/bobs$bob_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
