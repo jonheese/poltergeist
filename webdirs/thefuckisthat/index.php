aae624a0f697e058ea3de6188122ca9ff9395885
Jon Heese <jheese@inetu.net>
2018-01-02 16:26:28 -0500

    Add new sites

---
+++ webdirs/thefuckisthat/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>The Fuck Is That!?</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/thefuckisthat/thefuckisthat.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
