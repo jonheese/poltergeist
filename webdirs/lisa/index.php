69f3040618f607a10f822c5975d8abd66be12b8b
Jon Heese <jheese@inetu.net>
2018-01-03 08:44:58 -0500

    Fix lisa index

--- webdirs/lisa/index.php
+++ webdirs/lisa/index.php
@@ -1,10 +1,10 @@
 <html>
 	<head>
-		<title>The Fuck Is That!?</title>
+		<title>Lisa, It's Your Birthday!</title>
 	</head>
 	<body>
 <?php
-exec('/usr/bin/play /var/www/thefuckisthat/thefuckisthat.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+exec('/usr/bin/play /var/www/lisa/lisa.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
 ?>
 	</body>
 </html>
