90bed4382569438f7719010e96c712cc8be6c127
Jon Heese <jheese@inetu.net>
2018-01-10 15:24:12 -0500

    Add socoldlisa site

--- addsite.sh
+++ addsite.sh
@@ -13,3 +13,25 @@ mv webdirs/${site_name}.mp3 webdirs/$site_name/${site_name}.mp3
 
 cp apache-confs/site.conf.template apache-confs/${site_name}.conf
 sed -i "s/@site@/$site_name/g" apache-confs/${site_name}.conf
+
+echo -n "Do you have any customizations to make before commit/push? [y/N]: "
+read custom
+
+if [ "$custom" == "y" or "$custom" == "Y" ] ; then
+    echo "Please do the following steps manually after customization is complete:"
+    echo "   git commit -a"
+    echo "   git push -u origin master"
+    echo "   /root/jh/bin/deploy-poltergeist.sh"
+    exit 0
+fi
+
+git commit -a
+if [ $? -ne 0 ] ; then
+    echo "Error committing changes.  Please do the following steps manually:"
+    echo "   git commit -a"
+    echo "   git push -u origin master"
+    echo "   /root/jh/bin/deploy-poltergeist.sh"
+    exit 1
+fi
+git push -u origin master
+/root/jh/bin/deploy-poltergeist.sh
