9b55729f35c5e82f51bd90cc23dc1d0b99603f3a
Jon Heese <jheese@inetu.net>
2018-01-15 13:55:57 -0500

    Add doit and sonofa sites

---
+++ webdirs/sonofa/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>You son of a bitch!</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/sonofa/sonofa.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
