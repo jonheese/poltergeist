a850180706dd0afd5ccc4628cbf080aadce419a9
Jon Heese <jheese@inetu.net>
2018-01-15 13:58:37 -0500

    Add tumor site

---
+++ webdirs/tumor/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>It's Not a Tumor!</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/tumor/tumor.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
