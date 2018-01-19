be242fbf9849f606c11aa13a97fdd4326d1174d6
Jon Heese <jheese@inetu.net>
2018-01-19 07:22:23 -0500

    Add wilson site

---
+++ webdirs/wilson/index.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Wilson!</title>
+	</head>
+    <body>
+		<p align="center"><img src='wilson.png' /></p>
+<?php
+$wilson_number = rand(1,16);
+exec("/usr/bin/play /var/www/wilson/wilson$wilson_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
+?>
+	</body>
+</html>
