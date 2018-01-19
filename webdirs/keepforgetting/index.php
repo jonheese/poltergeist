f5bd1726be8c22d4c4b522edd848a9ddc67de90d
Jon Heese <jheese@inetu.net>
2018-01-19 14:24:45 -0500

    Add keepforgetting and regulate sites

---
+++ webdirs/keepforgetting/index.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<title>I Keep Forgetting</title>
+	</head>
+	<body>
+<?php
+exec('/usr/bin/play /var/www/keepforgetting/keepforgetting.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
