e96bb0dd695dcc7666974c09d121d3d47ab9943a
Jon Heese <jheese@inetu.net>
2018-01-19 13:45:49 -0500

    Normalize wilson mp3 files

--- webdirs/wilson/index.php
+++ webdirs/wilson/index.php
@@ -5,6 +5,7 @@
     <body>
 		<p align="center"><img src='wilson.png' /></p>
 <?php
+srand();
 $wilson_number = rand(1,16);
 exec("/usr/bin/play /var/www/wilson/wilson$wilson_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
 ?>
