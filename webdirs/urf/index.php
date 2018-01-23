afc9c48b2a61f5c2587b2730d3ed1af48dd2360e
Jon Heese <jheese@inetu.net>
2018-01-23 15:35:13 -0500

    Add site urf

---
+++ webdirs/urf/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Welcome to Urf!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/urf/urf.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
