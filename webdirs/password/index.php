3075cc19faa2710f5bddc0d51319e563feb76f03
Jon Heese <jheese@inetu.net>
2018-01-23 11:03:09 -0500

    Add password site

---
+++ webdirs/password/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>The Password is... Nipple</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/password/password.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
