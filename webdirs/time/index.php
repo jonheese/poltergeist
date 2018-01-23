01b47ec49e83ac45a0a8ee5309e8b0908ac9ef1e
Jon Heese <jheese@inetu.net>
2018-01-23 11:26:48 -0500

    Add time site

---
+++ webdirs/time/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Time to Have Sex!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/time/time.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
