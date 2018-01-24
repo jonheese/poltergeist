a57aa6acab6bda7e69781c60d3034d9312f42d61
Jon Heese <jheese@inetu.net>
2018-01-24 14:02:00 -0500

    Add isaidbitch site

---
+++ webdirs/isaidbitch/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>I Said Biiiiiiiiiiitch!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/isaidbitch/isaidbitch.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
