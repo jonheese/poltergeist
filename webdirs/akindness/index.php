f3f74ff4a293b9685c01e638ddea96232fc82f05
Jon Heese <jheese@inetu.net>
2018-01-29 15:43:23 -0500

    Fix akindness site

--- webdirs/akindness/index.php
+++ webdirs/akindness/index.php
@@ -5,7 +5,7 @@
 	<body>
 <!--		<p align="center"><img src='@imgfile@' /></p>-->
 <?php
-exec('/usr/bin/play /var/www/kindness/kindness.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+exec('/usr/bin/play /var/www/akindness/akindness.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
 ?>
 	</body>
 </html>
