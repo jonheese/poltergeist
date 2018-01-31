cbee117b2ab01600e63a0c7ea1dc575cceb63c33
Jon Heese <jheese@inetu.net>
2018-01-31 08:15:49 -0500

    Add remix site

---
+++ webdirs/remix/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>It's So Cold in the D (Remix)</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/remix/remix.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
