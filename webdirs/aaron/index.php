fcacd33d4905963fdc439e8ba8de293027548755
Jon Heese <jheese@inetu.net>
2018-01-31 11:30:24 -0500

    Add aaron site

---
+++ webdirs/aaron/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>You Done Messed Up, A-A-Ron!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/aaron/aaron.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
