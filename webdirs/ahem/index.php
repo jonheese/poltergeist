f9ed13bc3f0726b5ca60afd4629c00dd68c5d1c1
Jon Heese <jheese@inetu.net>
2018-02-05 13:08:43 -0500

    Add some new sites

---
+++ webdirs/ahem/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Let Me Clear My Throat</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/ahem/ahem.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
---
+++ webdirs/honeybadger/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Honey Badger Don't Care!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/honeybadger/honeybadger.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
