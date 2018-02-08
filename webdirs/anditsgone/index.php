1fccbccbcd8e01916ed0fd1acd415b5bcfc65711
Jon Heese <jheese@inetu.net>
2018-02-08 09:48:23 -0500

    Add anditsgone site

---
+++ webdirs/anditsgone/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Aaaaaaand, It's Gone!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/anditsgone/anditsgone.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
