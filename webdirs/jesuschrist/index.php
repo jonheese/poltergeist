11813292de01f58e3987ed487c754cc0cab7ca53
Jon Heese <jheese@inetu.net>
2018-02-13 15:58:16 -0500

    Add jesuschrist site

---
+++ webdirs/jesuschrist/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Jesus Christ!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/jesuschrist/jesuschrist.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
