a658b3c06e23c3f346c911a58e6ace18a00cbebc
Jon Heese <jheese@inetu.net>
2018-02-15 16:28:57 -0500

    Make itsfriday site only work on Fridays

--- webdirs/itsfriday/index.php
+++ webdirs/itsfriday/index.php
@@ -3,9 +3,13 @@
 		<title>It's Friday!</title>
 	</head>
 	<body>
-<!--		<p align="center"><img src='@imgfile@' /></p>-->
 <?php
-exec('/usr/bin/play /var/www/itsfriday/itsfriday.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+$day = date('l');
+if ( $day == "Friday" ) {
+    exec('/usr/bin/play /var/www/itsfriday/itsfriday.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+} else {
+	print "<p align='center'><img src='stahp.jpg' /></p>\n";
+}
 ?>
 	</body>
 </html>
