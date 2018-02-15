6bf4dbe097982662f183110c12f7201dc29fa8dd
Jon Heese <jheese@inetu.net>
2018-02-15 09:26:03 -0500

    Add chance site

---
+++ webdirs/chance/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>So You're Tellin' Me There's a Chance</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/chance/chance.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
