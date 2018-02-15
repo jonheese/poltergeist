35176bad4b4a868aa6d42cfbfaa05c259afbc485
Jon Heese <jheese@inetu.net>
2018-02-15 15:48:07 -0500

    Add shitright site

---
+++ webdirs/shitright/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>You Ain't Gonna Shit Right For a Week!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/shitright/shitright.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
