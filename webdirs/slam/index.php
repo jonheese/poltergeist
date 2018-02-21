c912ba7b21f171b6d9bad6de4c2063b842964dde
Jon Heese <jheese@inetu.net>
2018-02-21 08:05:51 -0500

    Add new mp3 to slam

--- webdirs/slam/index.php
+++ webdirs/slam/index.php
@@ -1,11 +1,11 @@
 <html>
 	<head>
-		<title>Don't Slam the Fuckin' Door!</title>
+		<title>Don't Slam the Door!</title>
 	</head>
 	<body>
-<!--		<p align="center"><img src='@imgfile@' /></p>-->
 <?php
-exec('/usr/bin/play /var/www/slam/slam.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+$mp3_number = rand(1,2);
+exec("/usr/bin/play /var/www/slam/slam$mp3_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
 ?>
 	</body>
 </html>
