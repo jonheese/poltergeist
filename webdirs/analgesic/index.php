bfb84d48582f816f0df57a752f37730742346420
Jon Heese <jheese@inetu.net>
2018-03-02 08:19:54 -0500

    Add analgesic site

---
+++ webdirs/analgesic/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/analgesic/analgesic.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
