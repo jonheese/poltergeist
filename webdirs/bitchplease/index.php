6b9b69b5c07481b92eb309debb184319cfb0d2f7
Jon Heese <jheese@inetu.net>
2018-03-13 09:56:26 -0400

    Add bitchplease site

---
+++ webdirs/bitchplease/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Bitch Please!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/bitchplease/bitchplease.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
