5a1b3d866887ca8ca61f88cb7bbd4936ce152393
Jon Heese <jheese@inetu.net>
2018-03-13 13:24:26 -0400

    Remove Frozen sound clip from slam

--- webdirs/slam/index.php
+++ webdirs/slam/index.php
@@ -4,8 +4,7 @@
 	</head>
 	<body>
 <?php
-$mp3_number = rand(1,2);
-exec("/usr/bin/play /var/www/slam/slam$mp3_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
+exec("/usr/bin/play /var/www/slam/slam1.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
 ?>
 	</body>
 </html>
