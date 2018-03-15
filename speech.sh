080b0d0dda507c78e676c4a18f4f54a08567d582
Jon Heese <jheese@inetu.net>
2018-03-15 08:17:04 -0400

    Fix speech.sh so it works better with 100+ character strings

--- speech.sh
+++ speech.sh
@@ -1,8 +1,70 @@
 #!/bin/bash
 
-INPUT=$*
-STRINGNUM=0
+GOOGLE_TRANSLATE_LIMIT=0
 
+function download_sound_file() {
+	INPUT=$*
+	if [ ${#INPUT} -lt 100 ] || [ $GOOGLE_TRANSLATE_LIMIT -lt 1 ] ; then
+		download_single_file $INPUT
+	else
+		download_multiple_files $INPUT
+	fi
+}
+
+function download_multiple_files() {
+	STRINGNUM=0
+	ary=($*)
+	for key in "${!ary[@]}" ; do
+		SHORTTMP[$STRINGNUM]="${SHORTTMP[$STRINGNUM]} ${ary[$key]}"
+		LENGTH=$(echo ${#SHORTTMP[$STRINGNUM]})
+		if [[ "$LENGTH" -lt "100" ]]; then
+			SHORT[$STRINGNUM]=${SHORTTMP[$STRINGNUM]}
+		else
+			STRINGNUM=$(($STRINGNUM+1))
+			SHORTTMP[$STRINGNUM]="${ary[$key]}"
+			SHORT[$STRINGNUM]="${ary[$key]}"
+		fi
+	done
+
+	rm -f ${output}*.mp3
+	keynum=0
+	for key in "${!SHORT[@]}" ; do
+		NEXTURL=$(echo ${SHORT[$key]} | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+		if [ $UID -eq 0 ] ; then
+			sudo -u $user wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$NEXTURL&tl=En-us"
+		else
+			wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$NEXTURL&tl=En-us"
+		fi
+		if [ $? -ne 0 ] ; then
+			echo "Unable to play $NEXTURL"
+			wgetcmd=wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=rururururururururururu&tl=En-us"
+			if [ $UID -eq 0 ] ; then
+				sudo -u $user $wgetcmd
+			else
+				$wgetcmd
+			fi
+		fi
+		keynum=$(($keynum+1))
+	done
+
+	catcmd="/bin/cat ${output}*.mp3"
+	if [ $UID -eq 0 ] ; then
+		sudo -u $user $catcmd > ${output}.mp3
+	else
+		$catcmd > ${output}.mp3
+	fi
+}
+
+function download_single_file() {
+	query=$(echo $* | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
+	if [ $UID -eq 0 ] ; then
+		sudo -u $user wget -q -U "Mozilla" -O ${output}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$query&tl=En-us"
+	else
+		wget -q -U "Mozilla" -O ${output}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$query&tl=En-us"
+	fi
+}
+
+INPUT=$*
 user="www-data"
 
 if [ "$(realpath -s $0)" == "/root/speech.sh" ] ; then
@@ -18,51 +80,8 @@ if [ -z "$INPUT" ] ; then
 fi
 
 export DISPLAY="[0]:0"
-ary=($INPUT)
-for key in "${!ary[@]}" ; do
-	SHORTTMP[$STRINGNUM]="${SHORTTMP[$STRINGNUM]} ${ary[$key]}"
-	LENGTH=$(echo ${#SHORTTMP[$STRINGNUM]})
-	#echo "word:$key, ${ary[$key]}"
-	#echo "adding to: $STRINGNUM"
-	if [[ "$LENGTH" -lt "100" ]]; then
-		#echo starting new line
-		SHORT[$STRINGNUM]=${SHORTTMP[$STRINGNUM]}
-	else
-		STRINGNUM=$(($STRINGNUM+1))
-		SHORTTMP[$STRINGNUM]="${ary[$key]}"
-		SHORT[$STRINGNUM]="${ary[$key]}"
-	fi
-done
+download_sound_file "$INPUT"
 
-rm -f ${output}*.mp3
-keynum=0
-for key in "${!SHORT[@]}" ; do
-	#echo "line: $key is: ${SHORT[$key]}"
-	#echo "Getting line: $(($key+1)) of $(($STRINGNUM+1))"
-	NEXTURL=$(echo ${SHORT[$key]} | xxd -plain | tr -d '\n' | sed 's/\(..\)/%\1/g')
-	if [ $UID -eq 0 ] ; then
-		sudo -u $user wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$NEXTURL&tl=En-us"
-	else
-		wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=$NEXTURL&tl=En-us"
-	fi
-	if [ $? -ne 0 ] ; then
-		echo "Unable to play $NEXTURL"
-		wgetcmd=wget -q -U "Mozilla" -O ${output}${keynum}.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&q=rururururururururururu&tl=En-us"
-		if [ $UID -eq 0 ] ; then
-			sudo -u $user $wgetcmd
-		else
-			$wgetcmd
-		fi
-	fi
-	keynum=$(($keynum+1))
-done
-
-catcmd="/usr/bin/sox ${output}*.mp3 ${output}.mp3"
-if [ $UID -eq 0 ] ; then
-	sudo -u $user $catcmd
-else
-	$catcmd
-fi
 date=$(date)
 echo "${date} - ${PROGNAME}: $*" >> /var/log/speech.log
 
