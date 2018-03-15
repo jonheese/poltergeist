40ee4179db8c47c58aa2d6bd6e576fd65ba232a3
Jon Heese <jheese@inetu.net>
2018-03-15 09:03:44 -0400

    Add horns site

---
+++ webdirs/horns/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Earth, Wind @title@ Fire - Got To Get You Into My Life</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/horns/horns.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
