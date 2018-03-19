9d27f61a0d96d35c891cac7bc648a6b3682c1742
Jon Heese <jheese@inetu.net>
2018-03-19 09:40:12 -0400

    Fix insult site

--- webdirs/insult/index.php
+++ webdirs/insult/index.php
@@ -3,9 +3,9 @@
 		<title>Winnie Hicks - Flatliners Insults</title>
 	</head>
 	<body>
-<!--		<p align="center"><img src='@imgfile@' /></p>-->
 <?php
-exec('/usr/bin/play /var/www/insult/insult.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+$mp3_number = rand(1,14);
+exec("/usr/bin/play /var/www/insult/insult$mp3_number.mp3 pad 30000s@0:00 >/dev/null 2>&1 &");
 ?>
 	</body>
 </html>
