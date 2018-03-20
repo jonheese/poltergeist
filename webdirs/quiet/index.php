c71b40ee9a2976f1c31e8fa2c1c77b3ff84c4225
Jon Heese <jheese@inetu.net>
2018-03-20 14:16:42 -0400

    "Fix" quiet site

--- webdirs/quiet/index.php
+++ webdirs/quiet/index.php
@@ -1,13 +1,12 @@
 <html>
-        <head>
-                <link rel="shortcut icon" href="stahp.jpg" />
-                <title>Vitas</title>
-        </head>
-        <body>
-                <p align="center"><img height='100%' src='stahp.jpg' /></p>
+	<head>
+		<link rel="shortcut icon" href="stahp.jpg" />
+		<title>Vitas</title>
+	</head>
+	<body>
+		<p align="center"><img height='100%' src='stahp.jpg' /></p>
 <?php
-$cmd = exec('ps -ef | grep play | grep -v grep | tr -s " " | cut -d" " -f8-');
-exec($cmd);
+exec('/usr/bin/killall play >/dev/null 2>&1 &');
 ?>
-        </body>
+	</body>
 </html>
