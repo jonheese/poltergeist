2ff6afae50c4a0bdeee9e4222429cb600e24472c
Jon Heese <jheese@inetu.net>
2018-03-23 15:34:16 -0400

    Add  site

--- addsite.sh
+++ addsite.sh
@@ -5,10 +5,15 @@ read site_name
 echo -n "Enter page title: "
 read page_title
 
-mkdir webdirs/$site_name
-cp webdirs/index.php.template webdirs/$site_name/index.php
-sed -i "s/@site@/$site_name/g" webdirs/${site_name}/index.php
-sed -i "s/@title@/$page_title/g" webdirs/${site_name}/index.php
+if [ -n "$sitename" ] ; then
+    mkdir webdirs/$site_name
+    cp webdirs/index.php.template webdirs/$site_name/index.php
+    sed -i "s/@site@/$site_name/g" webdirs/${site_name}/index.php
+    sed -i "s/@title@/$page_title/g" webdirs/${site_name}/index.php
+    mv webdirs/${site_name}.mp3 webdirs/$site_name/${site_name}.mp3
+    cp apache-confs/site.conf.template apache-confs/${site_name}.conf
+    sed -i "s/@site@/$site_name/g" apache-confs/${site_name}.conf
+fi
 
 if [ -f webdirs/${site_name}.jpg ] ; then
     imgfilepath="webdirs/${site_name}.jpg"
@@ -23,11 +28,6 @@ if [ -n "$imgfilepath" ] ; then
     sed -i "s/\-\->//g" webdirs/${site_name}/index.php
     mv $imgfilepath webdirs/${site_name}/
 fi
-
-mv webdirs/${site_name}.mp3 webdirs/$site_name/${site_name}.mp3
-
-cp apache-confs/site.conf.template apache-confs/${site_name}.conf
-sed -i "s/@site@/$site_name/g" apache-confs/${site_name}.conf
 
 echo -n "Do you have any customizations to make before commit/push/deploy? [y/N]: "
 read custom
