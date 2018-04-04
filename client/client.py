133a6d1916a65c9e09073eb8ed7043fcc786de00
Jon Heese <jheese@inetu.net>
2018-04-04 15:40:37 -0400

    First commit of client/server architecture

---
+++ client/client.py
@@ -0,0 +1,57 @@
+import requests, json, os.path, sys
+from subprocess import call
+from glob import glob
+from random import randint
+
+def config_missing():
+    print "You must configure the \"server\" parameter in config.py"
+    sys.exit(1)
+
+
+def check_for_command(response, **kwargs):
+    global clip_dir, play_cmd
+    data = json.loads(response.text)
+    print json.dumps(data, indent=2)
+    if "clips" in data.keys():
+        for clip in data["clips"]:
+            print "clip %s is present" % clip
+            if os.path.isdir("%s/%s" % (clip_dir, clip)):
+                clip_file = "%s/%s/%s.mp3" % (clip_dir, clip, clip)
+                if not os.path.exists(clip_file):
+                    clips = glob("%s/%s/%s*.mp3" % (clip_dir, clip, clip))
+                    clip_file = clips[randint(0, len(clips))]
+                print "Playing %s" % clip_file
+                call([play_cmd, clip_file])
+
+
+if not os.path.isfile('config.json'):
+    config_missing()
+
+config = json.load(open('config.json'))
+
+if not config or "server" not in config.keys():
+    config_missing()
+
+server = config["server"]
+
+if "port" in config.keys():
+    port = config["port"]
+else:
+    port = 80
+
+if "client_id" in config.keys():
+    client_id = config["client_id"]
+else:
+    client_id = call(["hostname"])
+
+if "clip_dir" in config.keys():
+    clip_dir = config["clip_dir"]
+else:
+    clip_dir = "/var/www/"
+
+if "play_cmd" in config.keys():
+    play_cmd = config["play_cmd"]
+else:
+    play_cmd = "/usr/bin/play"
+
+requests.get("http://%s:%s/cmd/%s" % (server, port, client_id), hooks={'response':check_for_command})
---
+++ server/server.py
@@ -0,0 +1,39 @@
+import json, uuid
+from redis import Redis
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+redis = Redis()
+app.config['REDIS_QUEUE_KEY'] = 'poltergeist'
+qkey = app.config['REDIS_QUEUE_KEY']
+
+@app.route('/cmd/<client_id>', methods=['GET'])
+def get_commands(client_id):
+    target_key = '%s:clip:%s' % (qkey, client_id)
+    clips = []
+    quiet = False
+    for key in redis.scan_iter(target_key+":*"):
+        clip = redis.get(key)
+        if quiet or clip == "quiet":
+            quiet = True
+            clips = [ "quiet" ]
+        else:
+            clips.append(clip)
+        redis.delete(key)
+    return jsonify(clips = clips)
+
+
+@app.route('/', methods=['GET'])
+def submit_command():
+    domain = request.headers['Host']
+    if domain.endswith("tv.inetu.org"):
+        client_id = "inetu-hdmi19"
+    elif domain.endswith("tv.inetu.net"):
+        client_id = "inetu-hdmi13"
+    else:
+        return jsonify(status = "I couldn't find the client_id for the URL you requested"), 404
+    target_key = '%s:clip:%s:%s' % (qkey, client_id, uuid.uuid4())
+    clip_name = domain.split(".")[0]
+    redis.set(target_key, clip_name)
+    return jsonify(status = "Request to play %s on %s successfully queued" % (clip_name, client_id))
