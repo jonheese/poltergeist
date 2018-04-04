c08ad2277ca9c2f570ac955d8d1b260add25e2b6
Jon Heese <jheese@inetu.net>
2018-04-04 15:28:23 -0400

    Add clownpenis site

---
+++ webdirs/clownpenis/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>www.clownpenis.fart</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/clownpenis/clownpenis.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
