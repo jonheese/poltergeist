105990ff7a415dab45712d0329f3d879c0cd2a01
Jon Heese <jon.heese@viawest.com>
2018-04-05 16:14:49 -0400

    Fix quiet queue stuff

--- client/client.py
+++ client/client.py
@@ -8,58 +8,47 @@ def check_for_command(response, **kwargs):
 
 
 def do_check_for_command(data):
-    global clip_queue
+    global clip_dir, play_cmd, quiet_queue, debug, quiet
     if debug:
         print json.dumps(data, indent=2)
     if "clips" in data.keys():
         for clip in data["clips"]:
             if clip == "quiet":
-                clear_queue(clip_queue)
-            clip_queue.put(clip)
-
-
-def clear_queue(clip_queue):
-    while not clip_queue.empty():
-        try:
-            clip_queue.get(False)
-        except Empty:
-            continue
+                quiet_queue.put(clip)
+                print "Put %s in the queue" % clip
+            else:
+                if debug:
+                    print "clip %s command received" % clip
+                if os.path.isdir("%s/%s" % (clip_dir, clip)):
+                    clip_file = "%s/%s/%s.mp3" % (clip_dir, clip, clip)
+                    if not os.path.exists(clip_file):
+                        if debug:
+                            print "Is directory"
+                        clips = glob.glob("%s/%s/%s*.mp3" % (clip_dir, clip, clip))
+                        clip_file = clips[random.randint(0, len(clips)-1)]
+                    elif debug:
+                        print "Is file"
+                    if not quiet:
+                        print "Playing %s" % clip_file
+                    call("%s %s %s >/dev/null 2>&1" % (play_cmd, clip_file, play_options), shell=True)
+                elif debug:
+                    print "Couldn't find directory %s/%s" % (clip_dir, clip)
 
 
 def process_clip_queue(dummy):
-    global clip_dir, play_cmd, clip_queue
+    global clip_dir, play_cmd, quiet_queue, debug, quiet
     try:
         while True:
-            while not clip_queue.empty():
-                clip = clip_queue.get()
-                if debug:
-                    print "found clip %s" % clip
-                if clip == "quiet":
+            while not quiet_queue.empty():
                     if not quiet:
                         print "Killing all sounds"
-                    clear_queue(clip_queue)
                     try:
-                        call("/usr/bin/killall play")
-                    except OSError:
+                        call(["killall", "play"])
+                        quiet_queue.get()
+                    except OSError as e:
                         # We don't care if the killall command fails
+                        print e
                         pass
-                else:
-                    if debug:
-                        print "clip %s command received" % clip
-                    if os.path.isdir("%s/%s" % (clip_dir, clip)):
-                        clip_file = "%s/%s/%s.mp3" % (clip_dir, clip, clip)
-                        if not os.path.exists(clip_file):
-                            if debug:
-                                print "Is directory"
-                            clips = glob.glob("%s/%s/%s*.mp3" % (clip_dir, clip, clip))
-                            clip_file = clips[random.randint(0, len(clips)-1)]
-                        elif debug:
-                            print "Is file"
-                        if not quiet:
-                            print "Playing %s" % clip_file
-                        call("%s %s %s >/dev/null 2>&1" % (play_cmd, clip_file, play_options), shell=True)
-                    elif debug:
-                        print "Couldn't find directory %s/%s" % (clip_dir, clip)
     except Exception as e:
         if not quiet:
             print "Encountered exception in process_clip_queue:"
@@ -77,24 +66,24 @@ client_id = config["client_id"] if "client_id" in config else check_output("host
 clip_dir = config["clip_dir"] if "clip_dir" in config.keys() else "/var/www"
 play_cmd = config["play_cmd"] if "play_cmd" in config.keys() else "/usr/bin/play"
 play_options = config["play_options"] if "play_options" in config.keys() else "pad 30000s@0:00"
-debug = config["debug"] if "debug" in config.keys() else False
-quiet = config["quiet"] if "quiet" in config.keys() else False
+debug = bool(config["debug"]) if "debug" in config.keys() else False
+quiet = bool(config["quiet"]) if "quiet" in config.keys() else False
 
-clip_queue = Queue.Queue()
+quiet_queue = Queue.Queue()
 
 thread.start_new_thread(process_clip_queue, (None,))
 
 while True:
     try:
-        if not debug:
+        if not quiet:
             print "Connecting to %s on port %s as client_id %s" % (server, port, client_id)
         requests.get("http://%s:%s/cmd/%s" % (server, port, client_id), hooks={'response':check_for_command})
     except KeyboardInterrupt:
-        if not debug:
+        if not quiet:
             print "Received CTRL-C, exiting..."
         sys.exit(0)
     except Exception as e:
-        if not debug:
+        if not quiet:
             print "Received unexpected exception:"
             print e
             print "Reconnecting in 5 seconds..."
