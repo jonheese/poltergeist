d7106d1d81564dade84d003d53b76eada84a3e91
Jon Heese <jheese@inetu.net>
2018-04-10 08:24:05 -0400

    Add Alexa thing

---
+++ client/install-client.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+systemctl=$(which systemctl >/dev/null 2>&1)
+dirname=$(readlink -f $(dirname $0))
+
+if [ ! -f "$systemctl" ] ; then
+    echo "Sorry, this install script only works on SystemD-based machines."
+#    exit 1
+fi
+
+cp $dirname/poltergeist-client.service /lib/systemd/system/
+systemctl daemon-reload
+systemctl enable poltergeist-client.service
+
+echo "To start the poltergeist-client service run:"
+echo "   systemctl start poltergeist-client.service"
