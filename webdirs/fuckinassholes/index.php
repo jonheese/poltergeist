05167cf83b4404dc2a82d4ae14ed231af538a555
Jon Heese <jheese@inetu.net>
2018-04-19 16:23:49 -0400

    Add fuckintired site

---
+++ webdirs/fuckinassholes/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Fuckin' Assholes</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/fuckinassholes/fuckinassholes.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
---
+++ webdirs/fuckintired/index.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>I'm Fuckin' Tired of This Shit!</title>
+	</head>
+	<body>
+<!--		<p align="center"><img src='@imgfile@' /></p>-->
+<?php
+exec('/usr/bin/play /var/www/fuckintired/fuckintired.mp3 pad 30000s@0:00 >/dev/null 2>&1 &');
+?>
+	</body>
+</html>
