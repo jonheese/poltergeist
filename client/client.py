46093eedfe628865252e28ebdf703e3ca90abe55
Jon Heese <jheese@inetu.net>
2019-10-30 16:26:47 -0400

    Updated code to run on python3

--- client/client.py
+++ client/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S python -u
 
-import requests, json, os.path, sys, time, thread, Queue, glob, random
+import requests, json, os.path, sys, time, glob, random
 from subprocess import call, check_output
 from requests.exceptions import ReadTimeout
 
@@ -12,8 +12,8 @@ def do_command(data):
     global poltergeist_dir, clip_dir, play_cmd, play_unkillable_cmd, quiet_queue, verbosity, checkpoint
     clip_played = False
     if verbosity > 1:
-        print json.dumps(data, indent=2)
-    if "clips" in data.keys():
+        print(json.dumps(data, indent=2))
+    if "clips" in list(data.keys()):
         for clip in data["clips"]:
             timestamp = float(clip.split(":")[1])
             if timestamp <= checkpoint:
@@ -25,21 +25,21 @@ def do_command(data):
             clip = clip.split(":")[0]
             if clip == "quiet" or clip == "quiet_all":
                 quiet_queue.put(clip)
-                print "Put %s in the queue" % clip
+                print("Put %s in the queue" % clip)
             else:
                 if verbosity > 1:
-                    print "clip %s command received" % clip
+                    print("clip %s command received" % clip)
                 if os.path.isdir("%s%s%s" % (clip_dir, os.path.sep, clip)):
                     clip_file = "%s%s%s%s%s.mp3" % (clip_dir, os.path.sep, clip, os.path.sep, clip)
                     if not os.path.exists(clip_file):
                         if verbosity > 1:
-                            print "Is directory"
+                            print("Is directory")
                         clips = glob.glob("%s%s%s%s%s*.mp3" % (clip_dir, os.path.sep, clip, os.path.sep, clip))
                         clip_file = clips[random.randint(0, len(clips)-1)]
                     elif verbosity > 1:
-                        print "Is file"
+                        print("Is file")
                     if verbosity > 0:
-                        print "Playing %s" % clip_file
+                        print("Playing %s" % clip_file)
                     if clip == "inagaddadavida":
                         actual_play_cmd = play_unkillable_cmd
                     else:
@@ -47,7 +47,7 @@ def do_command(data):
                     call("%s %s %s" % (actual_play_cmd, clip_file, play_options), shell=True)
                 else:
                     if verbosity > 1:
-                        print "Couldn't find directory %s%s%s" % (clip_dir, os.path.sep, clip)
+                        print("Couldn't find directory %s%s%s" % (clip_dir, os.path.sep, clip))
                     for file_name in glob.glob("%s%sapache-confs%s*.conf" % (poltergeist_dir, os.path.sep, os.path.sep)):
                         with open(file_name) as f:
                             contents = f.read()
@@ -61,18 +61,18 @@ def do_command(data):
                                     clip_played = True
                                     break
                     if not clip_played:
-                        print "Couldn't find site for clip %s" % clip
+                        print("Couldn't find site for clip %s" % clip)
 
 
 def process_quiet_queue(dummy):
     global clip_dir, quiet_queue, kill_cmd, killall_cmd, verbosity
     if verbosity > 1:
-        print "Starting up quiet queue"
+        print("Starting up quiet queue")
     try:
         while True:
             while not quiet_queue.empty():
                     if verbosity > 0:
-                        print "Killing all sounds"
+                        print("Killing all sounds")
                     try:
                         clip = quiet_queue.get()
                         if clip == "quiet_all":
@@ -82,13 +82,13 @@ def process_quiet_queue(dummy):
                         call(full_kill_cmd)
                     except OSError as e:
                         # We don't care if the killall command fails
-                        print e
+                        print(e)
                         pass
             time.sleep(0.1)
     except Exception as e:
         if verbosity > 0:
-            print "Encountered exception in process_quiet_queue:"
-            print e
+            print("Encountered exception in process_quiet_queue:")
+            print(e)
 
 
 def get_config():
@@ -100,69 +100,78 @@ def get_config():
             if sys.platform == "win32":
                 appdata_dir = os.getenv('appdata')
                 if not os.path.isfile("%s\\.poltergeist\\config.json" % appdata_dir):
-                    print "You must configure %%appdata%%\\.poltergeist\\config.py -- see config-dist.json for an example"
+                    print("You must configure %%appdata%%\\.poltergeist\\config.py -- see config-dist.json for an example")
                     sys.exit(1)
                 else:
                     config_file = "%s\\.poltergeist\\config.json" % appdata_dir
             else:
                 home_dir = os.getenv('HOME')
                 if not os.path.isfile("%s/.poltergeist/config.json" % home_dir):
-                    print "You must configure $HOME/.poltergeist/config.py -- see config-dist.json for an example"
+                    print("You must configure $HOME/.poltergeist/config.py -- see config-dist.json for an example")
                     sys.exit(1)
                 else:
                     config_file = "%s/.poltergeist/config.json" % home_dir
         else:
             config_file = 'config.json'
 
-        print "Using config file %s" % config_file
+        print("Using config file %s" % config_file)
 
     config = json.load(open(config_file))
-    schema = config["schema"] if "schema" in config.keys() else "http"
-    server = config["server"] if "server" in config.keys() else "localhost"
-    port = config["port"] if "port" in config.keys() else 80
+    schema = config["schema"] if "schema" in list(config.keys()) else "http"
+    server = config["server"] if "server" in list(config.keys()) else "localhost"
+    port = config["port"] if "port" in list(config.keys()) else 80
     client_id = config["client_id"] if "client_id" in config else check_output("hostname")
-    poltergeist_dir = config["poltergeist_dir"] if "poltergeist_dir" in config.keys() else "/root/poltergeist"
-    clip_dir = config["clip_dir"] if "clip_dir" in config.keys() else "/var/www"
-    play_cmd = config["play_cmd"] if "play_cmd" in config.keys() else "/usr/bin/play"
-    play_unkillable_cmd = config["play_unkillable_cmd"] if "play_unkillable_cmd" in config.keys() else "/usr/bin/play-unkillable"
-    play_options = config["play_options"] if "play_options" in config.keys() else "pad 30000s@0:00 >/dev/null 2>&1"
-    kill_cmd = config["kill_cmd"] if "kill_cmd" in config.keys() else "play"
-    killall_cmd = config["killall_cmd"] if "killall_cmd" in config.keys() else "killall"
-    verbosity = int(config["verbosity"]) if "verbosity" in config.keys() else 1
-    if "debug" in config.keys() and bool(config["debug"]):
+    poltergeist_dir = config["poltergeist_dir"] if "poltergeist_dir" in list(config.keys()) else "/root/poltergeist"
+    clip_dir = config["clip_dir"] if "clip_dir" in list(config.keys()) else "/var/www"
+    play_cmd = config["play_cmd"] if "play_cmd" in list(config.keys()) else "/usr/bin/play"
+    play_unkillable_cmd = config["play_unkillable_cmd"] if "play_unkillable_cmd" in list(config.keys()) else "/usr/bin/play-unkillable"
+    play_options = config["play_options"] if "play_options" in list(config.keys()) else "pad 30000s@0:00 >/dev/null 2>&1"
+    kill_cmd = config["kill_cmd"] if "kill_cmd" in list(config.keys()) else "play"
+    killall_cmd = config["killall_cmd"] if "killall_cmd" in list(config.keys()) else "killall"
+    verbosity = int(config["verbosity"]) if "verbosity" in list(config.keys()) else 1
+    if "debug" in list(config.keys()) and bool(config["debug"]):
         verbosity = 2
-    if "quiet" in config.keys() and bool(config["quiet"]):
+    if "quiet" in list(config.keys()) and bool(config["quiet"]):
         verbosity = 0
 
 config_file = None
 get_config()
-quiet_queue = Queue.Queue()
-thread.start_new_thread(process_quiet_queue, (None,))
+if sys.version_info[0] >= 3:
+    import _thread, queue
+    quiet_queue = queue.Queue()
+    _thread.start_new_thread(process_quiet_queue, (None,))
+else:
+    import thread, Queue
+    quiet_queue = Queue.Queue()
+    thread.start_new_thread(process_quiet_queue, (None,))
 if verbosity > 0:
-    print "Verbosity set to %s" % verbosity
+    print("Verbosity set to %s" % verbosity)
 
 while True:
     try:
         get_config()
         if verbosity > 0:
-            print "Connecting to %s on port %s as client_id %s" % (server, port, client_id)
+            print("Connecting to %s on port %s as client_id %s" % (server, port, client_id))
         r = requests.get("%s://%s:%s/cmd/%s" % (schema, server, port, client_id), stream=True, timeout=600)
         if verbosity > 0:
-            print "Received bytes from server"
+            print("Received bytes from server")
         for json_data in r.iter_lines():
             if json_data:
-                thread.start_new_thread(do_command, (json.loads(json_data),))
+                if sys.version_info[0] >= 3:
+                    _thread.start_new_thread(do_command, (json.loads(json_data),))
+                else:
+                    thread.start_new_thread(do_command, (json.loads(json_data),))
     except ReadTimeout:
         # The connection is designed to timeout and refresh every 10 minutes
         if verbosity > 1:
-            print "Didn't receive a command in 10 minutes. Looping."
+            print("Didn't receive a command in 10 minutes. Looping.")
     except KeyboardInterrupt:
         if verbosity > 0:
-            print "Received CTRL-C, exiting..."
+            print("Received CTRL-C, exiting...")
         sys.exit(0)
     except Exception as e:
         if verbosity > 0:
-            print "Received unexpected exception:"
-            print e
-            print "Reconnecting in 5 seconds..."
+            print("Received unexpected exception:")
+            print(e)
+            print("Reconnecting in 5 seconds...")
         time.sleep(5)
