9aa76f267c2e2e0c56051db6540c050ff70a52e9
Jon Heese <jheese@inetu.net>
2019-11-07 15:10:30 -0500

    Make queue_name ad hoc

--- server/server.py
+++ server/server.py
@@ -11,9 +11,9 @@ app.config['REDIS_QUEUE_KEY'] = 'poltergeist'
 qkey = app.config['REDIS_QUEUE_KEY']
 
 
-@app.route('/cmd/<client_id>', methods=['GET'])
-def get_commands(client_id):
-    target_key = '%s:clip:%s' % (qkey, client_id)
+@app.route('/cmd/<queue_name>', methods=['GET'])
+def get_commands(queue_name):
+    target_key = '%s:clip:%s' % (qkey, queue_name)
     clips = []
     start_time = time.time()
     while len(clips) == 0 and time.time() - start_time < 60:
@@ -44,9 +44,10 @@ def robots():
            "Disallow: /"
 
 
+@app.route('/play/<clip_name>/<queue_name>', methods=['GET'])
 @app.route('/play/<clip_name>', methods=['GET'])
 @app.route('/', methods=['GET'])
-def submit_command(clip_name=None):
+def submit_command(clip_name=None, queue_name=None):
     try:
         user_agent = request.headers['User-Agent'].lower()
         if "bot" in user_agent and not user_agent.startswith("slackbot"):
@@ -54,13 +55,15 @@ def submit_command(clip_name=None):
     except KeyError:
         return ""
 
-    domain = request.headers['Host']
-    if domain.endswith("inetu.net"):
-        client_id = "inetu-hdmi13"
-    elif domain.endswith("inetu.org") or domain.endswith("jonheese.com"):
-        client_id = "inetu-hdmi19"
-    else:
-        return jsonify(status = "I couldn't find the client_id for the URL you requested"), 404
+    if not queue_name:
+        domain = request.headers['Host']
+        if domain.endswith("inetu.net"):
+            queue_name = "inetu-hdmi13"
+        elif domain.endswith("inetu.org") or domain.endswith("jonheese.com"):
+            queue_name = "inetu-hdmi19"
+        else:
+            return jsonify(status = "I couldn't find the queue_name for the URL you requested: %s" % domain), 404
+
 
     if not clip_name:
         clip_name = domain.split(".")[0]
@@ -69,7 +72,7 @@ def submit_command(clip_name=None):
             ((clip_name.lower() == "lastchristmas" or clip_name.lower() == "jinglebell") and datetime.today().month != 12) or \
             (clip_name.lower() == "mondays" and datetime.today().weekday() != 0):
         return '<html><body><p align="center"><img src="/static/stahp.jpg" /></p></body></html>'
-    put_command(client_id, clip_name)
+    put_command(queue_name, clip_name)
 
     try:
         return render_template('%s/index.html' % clip_name)
@@ -77,9 +80,9 @@ def submit_command(clip_name=None):
         return render_template("notfound.html")
 
 
-def put_command(client_id, clip_name):
+def put_command(queue_name, clip_name):
     timestamp = time.time()
-    target_key = '%s:clip:%s:%s' % (qkey, client_id, timestamp)
+    target_key = '%s:clip:%s:%s' % (qkey, queue_name, timestamp)
     redis.set(target_key, "%s:%s" % (clip_name, str(timestamp)))
 
 
